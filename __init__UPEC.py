d9e29dbdea6d6989ecf9e2644ae75afb77af9bf5
felipefr <felipe.figueredo-rocha@u-pec.fr>
2023-12-22 19:08:10 +0100

    version UPEC before vacance

---
+++ __init__UPEC.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 14:32:02 2023
+
+@author: felipe rocha
+
+This file is part of micmacsfenics, a FEniCs-based implementation of 
+two-level finite element simulations (FE2) using computational homogenization.
+
+Copyright (c) 2022-2023, Felipe Rocha.
+See file LICENSE.txt for license information. 
+Please cite this work according to README.md.
+Please report all bugs and problems to <felipe.figueredo-rocha@ec-nantes.fr>, or <f.rocha.felipe@gmail.com>
+"""
